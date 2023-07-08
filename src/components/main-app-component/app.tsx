a6fadb12309e33a29caf7ca4a92a7a4bd72aea47
Vladimir <yaschuk_vladimir@mail.ru>
2023-07-08 20:33:00 +0300

    creates main page

---
+++ src/components/main-app-component/app.tsx
@@ -0,0 +1,10 @@
+import MainPage from '../../pages/main-page/main-page';
+
+function MainApp(): JSX.Element {
+  return (
+    <MainPage />
+  );
+
+}
+
+export default MainApp;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import MainApp from './components/main-app-component/app';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <MainApp />
   </React.StrictMode>
 );
